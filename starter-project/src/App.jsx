c2252a146a5a1ea17f4a41594e7e9f0c632bda8a
Sam Bishara <sbishara12@gmail.com>
2025-06-26 10:50:16 -0400

    Completed lab 08

--- starter-project/src/App.jsx
+++ starter-project/src/App.jsx
@@ -17,45 +17,35 @@ function App() {
     {id: 3, author: `Scrooge`, content: `Investing in breadcrumbs`}
   ]);
 
+  const [showForm, setShowForm] = useState(true);
+
   function addPost(newPost) {
     setPosts([{id: Date.now(), ...newPost }, ...posts]);
   }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
     <DuckbookLayout>
       <WelcomeMessage />
       <DuckButton label = "Add Friend" />
       <DuckButton label = "Message" />
       <DuckButton label = "Waddle Away" />
       <hr />
-      <PostForm onSubmit = {addPost}/>
+      <button onClick = {() => setShowForm(!showForm)} style = {{
+        marginBottom: `irem`,
+        backgroundColor: `#2196f3`,
+        color: `white`,
+        border: `none`,
+        padding: `0.5rem 1rem`,
+        borderRadius: `4px`,
+        cursor: `pointer`
+      }}>
+        {showForm ? 'Hide Post Form' : 'Show Post Form'}
+      </button>
+      {showForm && <PostForm onSubmit = {addPost}/>}
       {posts.map(post => (
         <Post key = {post.id} author={post.author} content={post.content} />
       ))}
     </DuckbookLayout>
-    </>
   )
 }
 
