9614187d04a371791909d7be78a6ddf9d790ce6e
Sam Bishara <sbishara12@gmail.com>
2025-06-26 10:02:41 -0400

    Completed lab 4

--- starter-project/src/Post.jsx
+++ starter-project/src/Post.jsx
@@ -1,4 +1,7 @@
 export default function Post({author, content}) {
+    function handleLikeClick() {
+        console.log(`Liked post by ${author}`);
+    }
     return (
         <div style={{
             border: `ipx solid #ccc`,
@@ -9,6 +12,17 @@ export default function Post({author, content}) {
         }}>
             <strong>{author}</strong>
             <p>{content}</p>
+            <button onClick = {handleLikeClick} style = {{
+                marginTop: `0.5rem`,
+                backgroundColor: `#ffc107`,
+                color: `black`,
+                border: 'none',
+                padding: '0.5rem 1rem',
+                borderRadisu: '4px',
+                cursor: 'pointer'
+            }}>
+                Like
+                </button>
         </div>
     );
 }
